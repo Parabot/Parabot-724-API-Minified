4a04dfab88e79c44f18be10ea45949ecd3bc45a4
Clisprail <clisprail@parabot.org>
2014-04-12 00:50:27 +0200

    Added MenuAction callback

--- src/org/rev724/min/callback/MenuAction.java
+++ src/org/rev724/min/callback/MenuAction.java
@@ -2,4 +2,12 @@ package org.rev724.min.callback;
 
 public class MenuAction {
 
+	public static void intercept(org.rev724.min.accessors.MenuAction action,
+			int n, int n1) {
+		System.out.println("ID: " + action.getId() + " Action1: "
+				+ action.getAction1() + " Action2: " + action.getAction2()
+				+ " Action3: " + action.getAction3() + " Action4: "
+				+ action.getAction4() + " mouseX: " + n + " mouseY: " + n1);
+	}
+
 }
