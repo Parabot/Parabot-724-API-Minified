89695c26a3d9a58887d16df6716dde74255e99e0
Clisprail <clisprail@parabot.org>
2014-04-12 00:50:48 +0200

    Added accessors

---
+++ src/org/rev724/min/accessors/Client.java
@@ -0,0 +1,5 @@
+package org.rev724.min.accessors;
+
+public interface Client {
+
+}
---
+++ src/org/rev724/min/accessors/MenuAction.java
@@ -0,0 +1,15 @@
+package org.rev724.min.accessors;
+
+public interface MenuAction {
+	
+	public int getId();
+	
+	public int getAction1(); 
+	
+	public int getAction2();
+	
+	public int getAction3();
+	
+	public long getAction4();
+
+}
